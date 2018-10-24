179df3b5c781276bcea630daff843283eb7bf6dc
abhinuv <abhinuvpitale@gmail.com>
2018-10-23 23:11:21 -0700

    added pi

--- action.php
+++ action.php
@@ -4,9 +4,10 @@
  if (isset($_POST['field1']) && isset($_POST['field2'])) {
     $fh = fopen($path,"w");
     echo $fh;
-    $string = $_POST['field2'].' \n '.$_POST['field1'];
+    $string = $_POST['field2'].':'.$_POST['field1'];
     echo $string;
     fwrite($fh,$string); // Write information to the file
     fclose($fh); // Close the file
+	echo exec('python shruPi.py')
  }
 ?>
---
+++ shruPi.py
@@ -0,0 +1,126 @@
+# Copyright (c) 2017 Adafruit Industries
+# Author: Tony DiCola & James DeVito
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# Edit by : Abhinuv Pitale https://abhinuvpitale.github.io
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+import RPi.GPIO as GPIO
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+
+import json, os, random
+import util
+import sys
+
+
+
+# Input pins:
+L_pin = 27
+R_pin = 23
+C_pin = 4
+U_pin = 17
+D_pin = 22
+
+A_pin = 5
+B_pin = 6
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(A_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(B_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(L_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(R_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(U_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(D_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+GPIO.setup(C_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Input with pull-up
+
+# Raspberry Pi pin configuration:
+RST = 24     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+#font = ImageFont.truetype('pixelmix.ttf', 8)
+print 'Loading Fonts'
+# Var Inits
+f = open('/var/www/html/data.txt')
+contents = f.read()
+contents = contents.split(":")
+quote = util.convertToDisplay(contents[1], 17)
+name = contents[0]
+print quote
+max_len = len(quote)
+count  = 0
+
+if max_len > 0:
+	draw.text((5,0),str(quote[0]),font=font,fill=255)
+if max_len > 1:
+	draw.text((5,8),str(quote[1]),font=font,fill=255)
+if max_len > 2:
+	draw.text((5,16),str(quote[2]),font=font,fill=255)
+draw.text((5,24),"- "+str(name),font=font,fill=255)
+
+disp.image(image)
+disp.display()
+
+
